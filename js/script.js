43e0c19dfa1e9b7d3dd0509a443dae0dfeec48c4
JuanP <jpchalaramirez@gmail.com>
2026-03-10 14:53:20 -0500

    2. Calculadora de suma

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ function mensaje() {
     var mensaje = document.getElementById("resultado")
     mensaje.innerHTML = "Hola mundo desde JavaScript"
 }
+
+// 2. Calculadora de suma
+function sumar() {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let resultado = (num1 + num2);
+    document.getElementById("resultadoSuma").innerHTML = "Suma: " + resultado;
+    // suma.innerHTML = "La suma es " + (resultado);
+}
